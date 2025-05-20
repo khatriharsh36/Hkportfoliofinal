1cf214fc3947ed1dc30ba8e13138b93b9c0e188d
khatriharsh36 <khatriharsh036@gmail.com>
2025-05-20 07:11:04 +0530

    Create google=maps.js

---
+++ assets/js/google=maps.js
@@ -0,0 +1,6 @@
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('google-maps'), {
+    center: { lat: 18.5204, lng: 73.8567 }, // Pune coordinates, change as needed
+    zoom: 12
+  });
+}
